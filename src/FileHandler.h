21af01baecaea8e32290444aa2173c4f6f669318
Chris <wiiqwertyuiop@gmail.com>
2023-03-12 17:17:51 -0400

    seperate

---
+++ src/FileHandler.h
@@ -0,0 +1,12 @@
+#ifndef FILEHANDLER_H // To make sure you don't declare the function more than once by including the header multiple times.
+#define FILEHANDLER_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<unsigned char> openFile(string);
+
+#endif
